8e90b21f776a2119deab366f47c2d40107964cd7
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2022-03-25 09:44:02 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Student account1=new Student("Jane Green",93.5);
+    Student account2=new Student("Jane Blue",72.75);
+
+    System.out.printf("%s's letter grade is : %s%n",
+                     account1.getName(),account1.getLetterGrage());
+    System.out.printf("%s's letter grade is : %s%n",
+                     account2.getName(),account2.getLetterGrage());
+  }
+}
